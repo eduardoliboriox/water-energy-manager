ebad7bb4a34604de2d847fd30b7411a325be6b90
Eduardo Soleno Moriz Liborio <eduardo.liborio@VENTTOS.local>
2025-11-24 22:42:33 -0400

    add

--- ping.py
+++ ping.py
@@ -1,12 +1,12 @@
 import time
 import requests
 
-# URL = "https://gerenciador-de-consumo-energia-agua.onrender.com"
+URL = "https://gerenciador-de-consumo-energia-agua.onrender.com"
 
-# while True:
-#     try:
-#         r = requests.get(URL)
-#         print("Ping OK:", r.status_code)
-#     except Exception as e:
-#         print("Erro:", e)
-#     time.sleep(300)  # 5 minutos
+while True:
+    try:
+        r = requests.get(URL)
+        print("Ping OK:", r.status_code)
+    except Exception as e:
+        print("Erro:", e)
+    time.sleep(300)  # 5 minutos
